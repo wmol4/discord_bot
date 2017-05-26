242b7d3eb1afc8842717936804f2ca8d3556d392
Wendell Luckow <wluckow@gmail.com>
2017-05-25 20:04:51 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,10 +11,15 @@ client = discord.Client()
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.startswith('back'):
-        random_length = randint(2, 10)
-        time.sleep(random_length)
-        msg = 'back'
-        await client.send_message(message.channel, msg)
+    if "back" in message.content.lower():
+        if message.author.voice.voice_channel != None:
+            voice_client = await client.join_voice_channel(message.author.voice_channel)
+            random_length = randint(2, 10)
+            time.sleep(random_length)
+            msg = 'back'
+            await client.send_message(message.channel, msg)
+            time.sleep(5)
+            await voice_client.disconnect()
+            
 
 client.run({token})
