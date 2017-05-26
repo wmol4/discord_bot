339cc20cd5666c24edc51ae8b592336926093c3a
Wendell Luckow <wluckow@gmail.com>
2017-05-25 20:21:03 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,17 +9,25 @@ client = discord.Client()
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
-    if "back" in message.content.lower():
-        if message.author.voice.voice_channel != None:
-            voice_client = await client.join_voice_channel(message.author.voice_channel)
-            random_length = randint(2, 10)
+    try:
+        if message.author == client.user:
+            return
+        if "back" in message.content.lower():
+            if message.author.voice.voice_channel != None:
+                random_length = randint(10, 30)
+                time.sleep(random_length)
+                voice_client = await client.join_voice_channel(message.author.voice_channel)
+                msg = 'back'
+                await client.send_message(message.channel, msg)
+                time.sleep(10)
+                await voice_client.disconnect()
+        if "loot" in message.content.lower():
+            random_length = randint(100, 300)
             time.sleep(random_length)
-            msg = 'back'
+            msg = '~loot'
             await client.send_message(message.channel, msg)
-            time.sleep(5)
-            await voice_client.disconnect()
+    except:
+        True
             
 
 client.run({token})
