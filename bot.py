b1cf6ad0f0ed6430845bdd6f585b42e794ae95f2
Wendell Luckow <wluckow@gmail.com>
2017-05-25 20:29:53 -0600

    Update

--- bot.py
+++ bot.py
@@ -7,12 +7,16 @@ from random import randint
 
 client = discord.Client()
 
+truthCounter1 = 1
+truthCounter2 = 1
+
 @client.event
 async def on_message(message):
     try:
         if message.author == client.user:
             return
-        if "back" in message.content.lower():
+        if "back" in message.content.lower() and truthCounter1 == 1:
+            truthCounter1 = 0
             if message.author.voice.voice_channel != None:
                 random_length = randint(10, 30)
                 time.sleep(random_length)
@@ -21,11 +25,14 @@ async def on_message(message):
                 await client.send_message(message.channel, msg)
                 time.sleep(10)
                 await voice_client.disconnect()
-        if "loot" in message.content.lower():
+                truthCounter1 = 1
+        if "loot" in message.content.lower() and truthCounter2 == 1:
+            truthCounter2 = 0
             random_length = randint(100, 300)
             time.sleep(random_length)
             msg = '~loot'
             await client.send_message(message.channel, msg)
+            truthCounter2 = 1
     except:
         True
             
